d6dd7016d62bbc0d08a6383e6ed374405cc419d5
greedJC <qweqwe472@naver.com>
2018-03-02 17:41:56 +0900

    '180302'

---
+++ bitcamp-java-basic/src/step01/Exam05_3.java
@@ -0,0 +1,17 @@
+// 정수 리터럴(literal) II - 자리수 표기
+package step01;
+
+public class Exam05_2 {
+    public static void main(String[] args) {
+        // _를 사용하여 읽기 쉽도록 숫자의 자리수를 표시할 수 있다.
+        // 물론 위치는 작성자 마음이다.
+        system.out.println(12_783_406);
+        system.out.println(12_783_406);
+
+       // 2진수 
+       // -0b 또는 0B로 시작한다.
+       System.out.println(0b1100100);
+       System.out.println(0b1_100_100);
+
+    }
+}
