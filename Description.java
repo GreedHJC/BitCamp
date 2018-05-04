c7cdfc48cc337488faca2e0d657a2c3c48c6ca52
GreedHJC <qweqwe472@naver.com>
2018-05-04 14:59:40 +0900

    180504

--- Description.java
+++
@@ -1,9 +0,0 @@
-package bitcamp.java106.pms;
-
-import java.util.Scanner;
-
-public abstract class Description {
-    public abstract void add(Scanner sc, String[] questions);
-    public abstract void get();
-    public abstract String getBase();
-}
